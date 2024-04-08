c7aab46df52ba153c977f614cf48157905d8e39b
Mark19971014 <s194751@student.dtu.dk>
2024-04-08 17:05:39 +0200

    complete page funcionality

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,7 +1,103 @@
 import React from 'react'
+import { Link ,useNavigate} from "react-router-dom";
+import { Label, TextInput, Button, Alert, Spinner } from "flowbite-react";
+import { useState } from "react";
 
 export default function SignIn() {
+  const [formData,SetFormData] = useState({});
+  const [errorMessage, setErrorMessage] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+  const handleChange = (e) => {
+      SetFormData({...formData, [e.target.id] : e.target.value.trim()})
+  };
+  // submit
+  const handleSubmit = async (e) =>{
+    e.preventDefault();
+
+    if(!formData.email || !formData.password){
+      return setErrorMessage('Please fill out all fields. ');
+    }
+    try {
+      setLoading(true);
+      setErrorMessage(null);
+      const res = await fetch('/api/auth/signin',{
+        method : 'POST',
+        headers : {'Content-Type':'application/json'},
+        body: JSON.stringify(formData),
+      });
+      
+      const data = await res.json();
+      if(data.success === false){
+        return setErrorMessage(data.message);
+      };
+      setLoading(false);
+      if(res.ok){
+        navigate('/');
+      }
+    } catch (error) {
+        setErrorMessage(error.message);
+        setLoading(false);
+
+    }
+  };
   return (
-    <div>SignIn</div>
-  )
+    <div className="min-h-screen mt-20 ">
+      <div className="flex p-3 max-w-3xl mx-auto flex-col md:flex-row md-items-center gap-5">
+        {/*left side*/}
+        <div className="flex-1">
+          <Link to="/" className="font-bold dark:text-white text-4xl">
+            <span
+              className="px-2 py-1 bg-gradient-to-r from-indigo-500 
+            via-purple-500 to-pink-500 rounded-lg text-white"
+            >
+              Qixin&nbsp;Ma's
+            </span>
+            Blog
+          </Link>
+          <p className="text-sm mt-5">
+            This is a demo project. You can sign in with your email and password
+            or with Google
+          </p>
+        </div>
+        {/*right side*/}
+        <div className="flex-1"></div>
+        <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
+          <div>
+            <Label value="Your email" />
+            <TextInput type="email" placeholder="name@company.com" id="email" onChange={handleChange}/>
+          </div>
+          <div>
+            <Label value="Your password" />
+            <TextInput type="password" placeholder="********" id="password" onChange={handleChange}/>
+          </div>
+          <Button gradientDuoTone="purpleToPink" type="submit" disabled = {loading}>
+            {
+            loading ? (
+              <>
+                <Spinner size = 'sm'/>
+                <span className = 'pl-3'>Loading...</span>
+              </>
+            ) : 'Sign In'
+}
+          </Button>
+        </form>
+        <div className="flex gap-2 text-sm mt-5">
+          <span>Don't an account?</span>
+          <Link to="/sign-up" className=" text-blue-500">
+            Sign Up
+          </Link>
+        </div>
+        <div>
+          {
+            errorMessage &&(
+              <Alert className= 'mt-5' color= 'failure'>
+                {errorMessage}
+              </Alert>
+            )
+          }
+        </div>
+      </div>
+    </div>
+  );
 }
