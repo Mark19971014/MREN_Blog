58db7e64c3da86d68cf5001395d50710a57b53f5
Mark19971014 <s194751@student.dtu.dk>
2024-04-19 15:23:04 +0200

    update header component with user data

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,10 +1,12 @@
-import { Navbar, TextInput, Button } from "flowbite-react";
+import { Navbar, TextInput, Button, Dropdown, Avatar } from "flowbite-react";
 import { Link, useLocation } from "react-router-dom";
 import { AiOutlineSearch } from "react-icons/ai";
 import { FaMoon } from "react-icons/fa";
+import {useSelector} from 'react-redux';
 
 export default function Header() {
   const path = useLocation.pathname;
+  const {currentUser} = useSelector(state => state.user)
   return (
     <Navbar className="border-b-2">
       <Link
@@ -35,11 +37,36 @@ export default function Header() {
         <Button className="w-12 h-10 hidden sm:inline" color="gray" pill>
           <FaMoon />
         </Button>
-        <Link to="/sign-in">
-          <Button gradientDuoTone="purpleToBlue" outline>
-            Sign In
-          </Button>
-        </Link>
+        {currentUser ? (
+          <Dropdown
+            arrowIcon = {false}
+            inline
+            label = {
+              <Avatar 
+                alt = 'user'
+                img = {currentUser.profilePicture}
+                rounded
+                />
+            }
+          >
+            <Dropdown.Header>
+            <span className="block text-sm">@{currentUser.username}</span>
+            <span className="block text-sm font-medium truncate">{currentUser.email}</span>
+            </Dropdown.Header>
+              <Link to ={'/dashboard ? tab = profile'}>
+                <Dropdown.Item>Profile</Dropdown.Item>
+              </Link>
+              <Dropdown.Divider/>
+              <Dropdown.Item>Sign out</Dropdown.Item>
+
+          </Dropdown>
+        ) :( <Link to="/sign-in">
+        <Button gradientDuoTone="purpleToBlue" outline>
+          Sign In
+        </Button>
+      </Link>
+        )
+      }
         <Navbar.Toggle />
       </div>
       {/*why we put Navbar.Collapse outside the div, what is div
