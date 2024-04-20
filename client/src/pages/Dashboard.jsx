2f2f08505ed21e30c8da6a96e2b387e558b5cbd1
Mark19971014 <s194751@student.dtu.dk>
2024-04-20 16:26:14 +0200

    complete profile page UI

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -15,9 +15,8 @@ export default function Dashboard() {
   return (
     <div className="min-h-screen flex flex-col md:flex-row">
       <div className="md:w-56">{/*sidebar*/ <Dashsidebar />}</div>
-        {/*profile...*/}
+      {/*profile...*/}
       {tab === "profile" && <DashProfile />}
-    
     </div>
   );
 }
