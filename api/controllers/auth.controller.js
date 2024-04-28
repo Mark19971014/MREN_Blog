afb66869576c4a8fd18bc2878c27c6fbe25dee0d
Mark19971014 <s194751@student.dtu.dk>
2024-04-28 19:28:54 +0200

    add admin functionality to the user

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -51,7 +51,7 @@ export const signin = async (req, res, next) => {
       }
 
     // authenticate the User
-    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+    const token = jwt.sign({ id: validUser._id, isAdmin: validUser.isAdmin }, process.env.JWT_SECRET);
       // hidden the password
     const  {password : pass,...rest} = validUser._doc;
     
@@ -70,7 +70,7 @@ export const google = async(req,res,next) =>{
   try{
       const user = await User.findOne({email});
       if(user){
-        const token = jwt.sign({id: user._id},process.env.JWT_SECRET);
+        const token = jwt.sign({id: user._id, isAdmin: user.isAdmin},process.env.JWT_SECRET);
         const{password,...rest} = user._doc;
         res.status(200).cookie('access_token',token,{
           httpOnly:true,
@@ -87,7 +87,7 @@ export const google = async(req,res,next) =>{
 
         });
         await newUser.save();
-        const token = jwt.sign({id: newUser._id},process.env.JWT_SECRET);
+        const token = jwt.sign({id: newUser._id, isAdmin: newUser.isAdmin},process.env.JWT_SECRET);
         const {password, ...rest} = newUser._doc;
         res.status(200).cookie('access_token',token,{
           httpOnly:true,
--- api/models/user.model.js
+++ api/models/user.model.js
@@ -19,6 +19,10 @@ const userSchema = new mongoose.Schema({
         type: String,
         default: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.freeiconspng.com%2Fimages%2Fprofile-icon-png&psig=AOvVaw1viIlm_glnnk3MqyDrPk2e&ust=1713190516717000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCNiPppTywYUDFQAAAAAdAAAAABAE",
     },
+    isAdmin:{
+        type:Boolean,
+        default:false,
+    },
     },
     {timestamps:true}
 );
