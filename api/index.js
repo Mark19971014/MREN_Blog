87ecae31c929496b31397483a60868ad6612c0de
Mark19971014 <s194751@student.dtu.dk>
2024-04-29 10:03:44 +0200

    add create a post api route

--- api/index.js
+++ api/index.js
@@ -3,6 +3,8 @@ import mongoose from "mongoose";
 import dotenv from 'dotenv';
 import userRoutes from './routes/user.route.js'
 import authRoutes from './routes/auth.route.js'
+import postRoutes from './routes/post.route.js'
+
 import cookieParser from "cookie-parser";
 dotenv.config()
 
@@ -22,8 +24,9 @@ app.listen(3000,() => {
     console.log('Server is running port 3000!')
 })
 
-app.use('/api/user',userRoutes)
-app.use('/api/auth',authRoutes)
+app.use('/api/user',userRoutes);
+app.use('/api/auth',authRoutes);
+app.use('/api/post',postRoutes);
 
 app.use((err,req,res,next) => {
     const statusCode = err.statusCode || 500;
